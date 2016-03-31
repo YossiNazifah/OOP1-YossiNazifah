ff4d525225c02b3f41f16d389079cbb2cad1b6e9
Yossi Nazifah <yossinazifah@gmail.com>
2016-03-31 09:03:06 +0700

    Added files via upload

---
+++ Biodata.java
@@ -0,0 +1,19 @@
+public class Biodata {
+    
+    public static void main(String[] args) {  
+        String nmA, nmB, tLhr, tglLhr, blnLhr, thnLhr;
+        nmA = "Yossi";
+        nmB = "Nazifah";
+        tLhr = "Kota Cimahi";
+        tglLhr = "11";
+        blnLhr = "Desember";
+        thnLhr = "1994";
+        
+        System.out.println("Nama Lengkap : " + nmA + " " + nmB);
+        System.out.println("Tempat Lahir : " + tLhr);
+        System.out.println("Tgl Lahir    : " + tglLhr);
+        System.out.println("Bln Lahir    : " + blnLhr);
+        System.out.println("Tahun Lahir  : " + thnLhr);
+        
+        }
+    }

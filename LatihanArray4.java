cea36d2cc5fd390c475a2e43cd767f17c16e395b
Yossi Nazifah <yossinazifah@gmail.com>
2016-04-01 18:53:48 +0700

    Added files via upload

---
+++ LatihanArray4.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanarray4;
+
+/**
+ *
+ * @author YOSSI
+ */
+public class LatihanArray4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     String a, b, c, d, e, f, g, h, i, j;
+     a = "Semangka";
+     b = "Pepaya";
+     c = "Jeruk";
+     d = "Apel";
+     e = "Mangga";
+     f = "Anggur";
+     g = "Strawberry";
+     h = "Cherry";
+     i = "Pir";
+     j = "Kelapa";
+     
+     String[] v = {"Bus", "Mobil", "BMW", "Truk", "Motor", "Beat", "Sepeda", "Scooter", "Becak", "Delman"};
+             
+             System.out.println("1. " + a);
+             System.out.println("2. " + b);
+             System.out.println("3. " + c);
+             System.out.println("4. " + d);
+             System.out.println("5. " + e);
+             System.out.println("6. " + f);
+             System.out.println("7. " + g);
+             System.out.println("8. " + h);
+             System.out.println("9. " + i);
+             System.out.println("10. " + j);
+             
+             System.out.println("1. " + v[0]);
+             System.out.println("2. " + v[1]);
+             System.out.println("3. " + v[2]);
+             System.out.println("4. " + v[3]);
+             System.out.println("5. " + v[4]);
+             System.out.println("6. " + v[5]);
+             System.out.println("7. " + v[6]);
+             System.out.println("8. " + v[7]);
+             System.out.println("9. " + v[8]);
+             System.out.println("10." + v[9]);
+    }
+    
+}

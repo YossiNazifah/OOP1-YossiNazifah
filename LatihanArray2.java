8b7e8cc80f26d446816668a29a9b27409beaab6c
Yossi Nazifah <yossinazifah@gmail.com>
2016-04-01 18:52:48 +0700

    Added files via upload

---
+++ LatihanArray2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanarray2;
+
+/**
+ *
+ * @author YOSSI
+ */
+public class LatihanArray2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 5;
+     int[] b = {100, 200, 300, 400};
+     
+     System.out.println(b[0]);
+     System.out.println(b[1]);
+     System.out.println(b[2]);
+     System.out.println(b[3]);
+     
+    }
+    
+}

fcd707912a0379b07ad3c893112bf3d74db3f8ca
Yossi Nazifah <yossinazifah@gmail.com>
2016-04-08 20:57:35 +0700

    Added files via upload

---
+++ LatihanBahasaJavaScanner.java
@@ -0,0 +1,16 @@
+package latihanbahasajavascanner;
+
+import java.util.Scanner;
+public class LatihanBahasaJavaScanner {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int bil, Jumlah, pil, data, total = 5;
+       System.out.println("Masukkan nilai N");
+       Jumlah = input.nextInt();
+             System.out.println("Masukkan Bilangan A atau Bilangan B");
+             bil = input.nextInt();
+             
+    }
+    
+}
